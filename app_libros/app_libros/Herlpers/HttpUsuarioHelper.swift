ca0c7a28ab4ea540e06b44202d2ac506c0f18d98
DianaM09 <i202030258@cibertec.edu.pe>
2024-12-18 11:36:27 -0500

    login y registro

---
+++ app_libros/app_libros/Herlpers/HttpUsuarioHelper.swift
@@ -0,0 +1,8 @@
+//
+//  HttpUsuarioHelper.swift
+//  applibros
+//
+//  Created by DAMII on 18/12/24.
+//
+
+import Foundation
---
+++ app_libros/app_libros/Views/RegistrerView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrerView.swift
+//  applibros
+//
+//  Created by DAMII on 18/12/24.
+//
+
+import SwiftUI
+
+struct RegistrerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrerView()
+}
